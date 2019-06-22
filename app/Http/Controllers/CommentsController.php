9d63bc0543e56498206077e92a23f920b78daeb6
me <me@me.com>
2019-06-23 02:19:12 +0600

    fixed indentation on controllers

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    public function store(Tweet $tweet){
-        auth()->user()->commentedTweets()->attach($tweet->id, ['body' => request('body')]);
-    }
+  public function store(Tweet $tweet){
+    auth()->user()->commentedTweets()->attach($tweet->id, ['body' => request('body')]);
+  }
 
-    public function destroy(Tweet $tweet, $commentId){
-        // auth()->user()->commentedTweets()->detach($tweet->id);
-        auth()->user()->commentedTweets()->where('tweet_id', $tweet->id)->wherePivot('id', '=', $commentId)->detach();
-    }
+  public function destroy(Tweet $tweet, $commentId){
+    // auth()->user()->commentedTweets()->detach($tweet->id);
+    auth()->user()->commentedTweets()->where('tweet_id', $tweet->id)->wherePivot('id', '=', $commentId)->detach();
+  }
 }
 
